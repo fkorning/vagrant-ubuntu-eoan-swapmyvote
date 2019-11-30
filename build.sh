b542ec828f040c386a4ef89e41302042c5de7afd
Francis Korning <fkorning@yahoo.ca>
2019-11-30 09:34:08 +0000

    adding db:migrate; comments
    adding db:migrate; comments

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-# steps to build on vagrant vm
+# steps for a clean local build with bundle and rails on vagrant vm
+# replace -b 10.0.2.15 with the NAT ip of your virtualbox guest vm
+# make sure you open-up the firewall ports and map NAT port forwards
 
 rm *.lock
-
 
 bundle clean --force
 
@@ -9,8 +10,10 @@ bundle
 bundle install
 bundle install --without=production
 
-bundle exec rails webpacker:install
-
 bundle exec rake db:setup
+
+bundle exec rails db:migrate
+
+bundle exec rails webpacker:install
 
 bundle exec rails server -b 10.0.2.15
